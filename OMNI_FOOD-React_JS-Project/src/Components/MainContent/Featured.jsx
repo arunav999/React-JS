634bfc534d2f6fafa8be916958645c79280a0e53
Arunav Singh <arunavsingh999@gmail.com>
2024-11-21 00:30:01 +0530

    Featured-in Section completed

--- OMNI_FOOD-React_JS-Project/src/Components/MainContent/Featured.jsx
+++ OMNI_FOOD-React_JS-Project/src/Components/MainContent/Featured.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
-const Featured = () => {};
+import BusinessInsider from "../../../public/logos/business-insider.png";
+import Forbes from "../../../public/logos/forbes.png";
+import TechCrunch from "../../../public/logos/techcrunch.png";
+import NewYorkTimes from "../../../public/logos/the-new-york-times.png";
+import UsaToday from "../../../public/logos/usa-today.png";
+
+const Featured = () => {
+  return (
+    <section className="section-featured">
+      <div className="container">
+        <h2 className="heading-featured-in">As featured in.</h2>
+        <div className="logos">
+          <img src={TechCrunch} alt="Techcrunch logo" />
+          <img src={BusinessInsider} alt="Business insider logo" />
+          <img src={NewYorkTimes} alt="The New York Times logo" />
+          <img src={Forbes} alt="Forbes logo" />
+          <img src={UsaToday} alt="USA Today logo" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Featured;
